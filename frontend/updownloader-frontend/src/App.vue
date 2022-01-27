e57e9270b67700e54a16de12e61cd40baaf69db4
poorpool <489679970@qq.com>
2022-01-27 14:41:35 +0800

    feat(frontend): base page

--- frontend/updownloader-frontend/src/App.vue
+++ frontend/updownloader-frontend/src/App.vue
@@ -1,15 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <el-container>
+    <el-header style="padding: 0;">
+      <el-menu mode="horizontal" >
+        <el-menu-item index="1">UPDOWNLOADER</el-menu-item>
+        <el-menu-item index="2">上传文字</el-menu-item>
+        <el-menu-item index="3">上传文件</el-menu-item>
+        <el-menu-item index="4">后台</el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-main>
+
+
+      <el-row gutter="5" justify="center" style="margin-bottom: 5px">
+        <el-col :xs="18" :sm="16" :md="12" :lg="12" :xl="12">
+          <el-input v-model="codeInput" placeholder="输入六位代码"
+                    size="large" maxlength="6" minlength="6"
+                    @keyup.enter="query"
+          />
+        </el-col>
+        <el-col :xs="6" :sm="4" :md="2" :lg="2" :xl="2">
+          <el-button type="primary" size="large" style="display: flex" @click="query">查询</el-button>
+        </el-col>
+      </el-row>
+
+      <el-row justify="center">
+        <el-col :xs="24" :sm="24" :md="20" :lg="18" :xl="16">
+          <el-card class="box-card" v-if="ifShowCard">
+            <template #header>
+              <div class="card-header">
+                <span>{{cardTitle}}</span>
+                <!--            <el-button class="button" type="text">Operation button</el-button>-->
+              </div>
+            </template>
+            <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+          </el-card>
+        </el-col>
+      </el-row>
+
+
+    </el-main>
+  </el-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      codeInput: "",
+      ifShowCard: false,
+      cardTitle: ""
+    }
+  },
+  methods: {
+    query() {
+      this.cardTitle = this.codeInput;
+      this.ifShowCard = true;
+    }
   }
 }
 </script>
@@ -19,8 +70,11 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
